024d0d51d0f71c5bec6c344ce2b863f9de49113e
Rosa <sandorosa1@gmail.com>
2017-06-06 19:49:23 -0600

    Suma dos digitos

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,25 @@ router.get('/0819199500053', function(req, res, next) {
     res.json(datos);
 });
 
+
+
+router.get('/sumas', function(req, res, next) {
+  res.render('suma', {"numeroUno":"","numeroDos":"","sumas":""});
+});
+
+var numerouno=[];
+var numerodos=[];
+
+router.post('/sumas', function(req, res, next) {
+    console.log(req.body);
+    numerouno.push(req.body.numeroUno);
+    numerodos.push(req.body.numeroDos);
+    var suma= parseInt(numerouno)+parseInt(numerodos);
+    var objeto={};
+    objeto.numeroUno=req.body.numeroUno;
+    objeto.numeroDos=req.body.numeroDos;
+    objeto.sumas=suma;
+    res.render('suma',objeto);
+});
+
 module.exports = router;
